bb5f9a22a05de178020fc41b770a31bafe89cd44
jml@canonical.com <>
2007-03-31 12:50:14 +1000

    Use underscores for naming methods, split out natural language stuff, move title_case to module level,  make tests less brittle.

--- testdoc/__init__.py
+++ testdoc/__init__.py
@@ -29,19 +29,32 @@ def gen_split_name(name,
                 yield bit.lower()
 
 
+def title_case(words):
+    titled = []
+    for word in words:
+        if word.lower() in ['in', 'a', 'the', 'of', 'has']:
+            titled.append(word.lower())
+        elif word.upper() == word:
+            titled.append(word)
+        else:
+            titled.append(word.capitalize())
+    titled = ' '.join(titled)
+    return titled[0].upper() + titled[1:]
+
+
 def get_lineno(obj):
     return inspect.getsourcelines(obj)[1]
 
 
 def find_tests(finder, module):
-    finder.gotModule(module)
+    finder.got_module(module)
     classes = sorted(reflect.findTestClasses(module), key=get_lineno)
     for testCaseClass in classes:
-        finder.gotTestClass(testCaseClass)
+        finder.got_test_class(testCaseClass)
         methods = [getattr(testCaseClass, 'test%s' % name)
                    for name in reflect.getTestCaseNames(testCaseClass)]
         for method in sorted(methods, key=get_lineno):
-            finder.gotTest(method)
+            finder.got_test(method)
 
 
 class Documenter(object):
@@ -49,40 +62,35 @@ class Documenter(object):
     def __init__(self, formatter):
         self.formatter = formatter
 
-    def titleCase(self, words):
-        titled = []
-        for word in words:
-            if word.lower() in ['in', 'a', 'the', 'of']:
-                titled.append(word.lower())
-            elif word.upper() == word:
-                titled.append(word)
-            else:
-                titled.append(word.capitalize())
-        titled = ' '.join(titled)
-        return titled[0].upper() + titled[1:]
+    def _append_docs(self, obj):
+        docs = self.extract_docs(obj)
+        if docs is not None:
+            self.formatter.paragraph(docs)
 
-    def getDocs(self, obj):
+    def extract_docs(self, obj):
         doc = inspect.getdoc(obj)
         if doc is None:
             doc = inspect.getcomments(obj)
         return doc
 
-    def gotModule(self, module):
+    def format_module(self, module_name):
+        return module_name
+
+    def format_test(self, test_name):
+        return title_case(split_name(test_name)[1:])
+
+    def format_test_class(self, class_name):
+        return title_case(
+            [bit for bit in split_name(class_name) if bit != 'test'])
+
+    def got_module(self, module):
         self.formatter.title(module.__name__)
-        docs = self.getDocs(module)
-        if docs is not None:
-            self.formatter.paragraph(docs)
+        self._append_docs(module)
 
-    def gotTestClass(self, klass):
-        self.formatter.section(self.titleCase(
-                [bit for bit in split_name(klass.__name__) if bit != 'test']))
-        docs = self.getDocs(klass)
-        if docs is not None:
-            self.formatter.paragraph(docs)
+    def got_test(self, method):
+        self.formatter.subsection(self.format_test(method.__name__))
+        self._append_docs(method)
 
-    def gotTest(self, method):
-        self.formatter.subsection(
-            self.titleCase(split_name(method.__name__)[1:]))
-        docs = self.getDocs(method)
-        if docs is not None:
-            self.formatter.paragraph(docs)
+    def got_test_class(self, klass):
+        self.formatter.section(self.format_test_class(klass.__name__))
+        self._append_docs(klass)
--- testdoc/tests/test_testdoc.py
+++ testdoc/tests/test_testdoc.py
@@ -1,34 +1,34 @@
 import inspect
 import unittest
 
-from testdoc import split_name, find_tests, Documenter
+from testdoc import split_name, find_tests, Documenter, title_case
 
 
 class TestSplitName(unittest.TestCase):
 
-    def test_singleWord(self):
+    def test_single_word(self):
         self.assertEqual(split_name('single'), ['single'])
 
     def test_underscores(self):
         self.assertEqual(split_name('split_name'), ['split', 'name'])
 
-    def test_camelCase(self):
+    def test_camel_case(self):
         self.assertEqual(split_name('splitName'), ['split', 'name'])
         self.assertEqual(
             split_name('splitLongName'), ['split', 'long', 'name'])
         self.assertEqual(split_name('splitAName'), ['split', 'a', 'name'])
 
-    def test_camelCaseWithCaps(self):
+    def test_camel_case_with_caps(self):
         self.assertEqual(split_name('splitDNSName'), ['split', 'DNS', 'name'])
 
-    def test_singleUnderscore(self):
+    def test_single_underscore(self):
         """Single underscores are used for reflection prefixes, so we'd like to
         ignore them.
         """
         self.assertEqual(
             split_name('test_splitName'), ['test', 'split', 'name'])
 
-    def test_multipleUnderscores(self):
+    def test_multiple_underscores(self):
         """If there are multiple underscores, but camel case, then someone is
         probably referring to a camel-cased identifier in their name.
         """
@@ -44,13 +44,13 @@ class MockFinder(object):
     def __init__(self):
         self.log = []
 
-    def gotModule(self, module):
+    def got_module(self, module):
         self.log.append(('module', module))
 
-    def gotTestClass(self, klass):
+    def got_test_class(self, klass):
         self.log.append(('class', klass))
 
-    def gotTest(self, method):
+    def got_test(self, method):
         self.log.append(('method', method))
 
 
@@ -109,43 +109,66 @@ class TestDocumenter(unittest.TestCase):
 
     def test_module(self):
         from testdoc.tests import empty
-        self.documenter.gotModule(empty)
+        self.documenter.got_module(empty)
         self.assertEqual(
-            self.formatter.log, [
-                ('title', 'testdoc.tests.empty')])
+            self.formatter.log,
+            [('title', self.documenter.format_module('testdoc.tests.empty'))])
 
-    def test_emptyModuleWithDocstrings(self):
+    def test_empty_module_with_docstrings(self):
         from testdoc.tests import hastests
-        self.documenter.gotModule(hastests)
+        self.documenter.got_module(hastests)
         self.assertEqual(
-            self.formatter.log, [
-                ('title', 'testdoc.tests.hastests'),
-                ('para', self.documenter.getDocs(hastests))])
+            self.formatter.log,
+            [('title',
+              self.documenter.format_module('testdoc.tests.hastests')),
+             ('para', self.documenter.extract_docs(hastests))])
 
-    def test_emptyCase(self):
+    def test_empty_case(self):
         from testdoc.tests import hastests
-        self.documenter.gotTestClass(hastests.SomeTest)
+        self.documenter.got_test_class(hastests.SomeTest)
         self.assertEqual(
-            self.formatter.log, [
-                ('section', self.documenter.titleCase(['Some'])),
-                ('para', self.documenter.getDocs(hastests.SomeTest))])
+            self.formatter.log,
+            [('section', self.documenter.format_test_class('SomeTest')),
+             ('para', self.documenter.extract_docs(hastests.SomeTest))])
 
     def test_method(self):
         from testdoc.tests import hastests
-        self.documenter.gotTest(hastests.SomeTest.test_foo_handles_qux)
+        self.documenter.got_test(hastests.SomeTest.test_foo_handles_qux)
         self.assertEqual(
-            self.formatter.log, [
-                ('subsection',
-                 self.documenter.titleCase(['Foo', 'handles', 'qux'])),
-                ('para', self.documenter.getDocs(
-                    hastests.SomeTest.test_foo_handles_qux))])
+            self.formatter.log,
+            [('subsection',
+              self.documenter.format_test('test_foo_handles_qux')),
+             ('para', self.documenter.extract_docs(
+            hastests.SomeTest.test_foo_handles_qux))])
 
-    def test_titleCase(self):
+    def test_title_case(self):
         self.assertEqual(
-            self.documenter.titleCase(['foo', 'BAR', 'a', 'In', 'Baz', '999']),
-            'Foo BAR a in Baz 999')
-        self.assertEqual(
-            self.documenter.titleCase(['in', 'a', 'bind']),
-            'In a Bind')
+            title_case(['foo', 'BAR', 'a', 'In', 'Baz', '999', 'has']),
+            'Foo BAR a in Baz 999 has')
+        self.assertEqual(title_case(['in', 'a', 'bind']), 'In a Bind')
 
-    # Docstrings first, then comments, then nothing
+    def test_format_module(self):
+        """The natural language display of a module name is just the name of
+        the module.
+        """
+        self.assertEqual('foo.bar.baz',
+                         self.documenter.format_module('foo.bar.baz'))
+
+    def test_format_test_class(self):
+        """The natural language display of a test class name is the class name
+        split up into words with title-case capitalization and with all
+        mentions of 'Test' stripped out.
+        """
+        self.assertEqual('Foo Bar',
+                         self.documenter.format_test_class('TestFooBar'))
+        self.assertEqual('Foo Bar',
+                         self.documenter.format_test_class('FooBarTest'))
+
+
+    def test_format_test(self):
+        """The natural language display of a test method name is the name split
+        into words with the initial word (usually 'test') dropped off. The
+        phrase uses title-case capitalization.
+        """
+        self.assertEqual('Janey has a Gun',
+                         self.documenter.format_test('test_janey_has_a_gun'))
